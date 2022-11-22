77ce0dc10aa8073cefc906206c6cb54324938a9a
Maximilian Heisinger <mail@maxheisinger.at>
2022-11-22 18:39:41 +0100

    Less function calls in eliminate_equivalence

--- src/transform/include/booleguru/transform/eliminate_equivalence.hpp
+++ src/transform/include/booleguru/transform/eliminate_equivalence.hpp
@@ -6,8 +6,10 @@ namespace booleguru::transform {
 struct eliminate_equivalence : public visitor<eliminate_equivalence> {
   inline op_ref walk_equi(op_ref e) {
     using namespace expression;
-    op_ref l_or_not_r = l(e) || !r(e);
-    op_ref not_l_or_r = !l(e) || r(e);
+    auto left = l(e);
+    auto right = r(e);
+    op_ref l_or_not_r = left || !right;
+    op_ref not_l_or_r = !left || right;
     return l_or_not_r && not_l_or_r;
   }
 };
