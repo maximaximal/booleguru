adae271b8f4143c46de965698997d6daa0b8ac24
Maximilian Heisinger <mail@maxheisinger.at>
2023-02-21 00:47:59 +0100

    Implement quantblocks lua function

--- src/lua/scripts/graph-printer.lua
+++ src/lua/scripts/graph-printer.lua
@@ -54,3 +54,19 @@ function dotter (op, out)
 
   o:write("}\n")
 end
+
+function quantblocks(op)
+  local o = op
+  local quantsyms = {}
+  quantsyms[optype.exists] = "?"
+  quantsyms[optype.forall] = "#"
+  while o.t == optype.forall or o.t == optype.exists do
+    local quants = quantsyms[o.t]
+    local t = o.t
+    while o.t == t do
+      quants = quants .. " " .. tostring(o.l)
+      o = o.r
+    end
+    print(quants)
+  end
+end
