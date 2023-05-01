c6e93e18f06c74d882d2ffa3f157b6345bf29336
Max Heisinger <maximaximal@SAIBookAir.simax.lan>
2023-05-01 12:38:58 +0200

    Add comment

--- src/transform/test/test_eliminate_implication.cpp
+++ src/transform/test/test_eliminate_implication.cpp
@@ -202,6 +202,8 @@ TEST_CASE("Simple Test 5") {
   REQUIRE(equiv_.to_string() == expected);
 }
 TEST_CASE("Simple Test 6", "[.]") {
+  // This requires some in-processing, which we will do later.
+
   op_manager ops;
 
   op_ref p = "p"_var(ops);
