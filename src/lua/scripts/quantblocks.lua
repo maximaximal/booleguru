2b42ec38cd0bb3787fc8e58432acae414e6354f3
Max Heisinger <maximaximal@SAIBookAir.simax.lan>
2023-05-01 15:24:13 +0200

    Improve the quantblocks lua script

--- src/lua/scripts/quantblocks.lua
+++ src/lua/scripts/quantblocks.lua
@@ -1,13 +1,13 @@
 function quantblocks(op)
   local o = op
   local quantsyms = {}
-  quantsyms[optype.exists] = "?"
-  quantsyms[optype.forall] = "#"
+  quantsyms[optype.exists] = "∃"
+  quantsyms[optype.forall] = "∀"
   while o.t == optype.forall or o.t == optype.exists do
-    local quants = quantsyms[o.t]
+    local quants = ""
     local t = o.t
     while o.t == t do
-      quants = quants .. " " .. tostring(o.l)
+      quants = quants .. " " .. quantsyms[o.t] .. tostring(o.l)
       o = o.r
     end
     print(quants)
