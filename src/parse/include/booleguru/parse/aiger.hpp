4c288ff0ae79fceb6a2ad55b34c60537359c4e09
Max Heisinger <mail@maxheisinger.at>
2023-08-09 22:52:20 +0200

    [AIGER] Progress, but not fully there yet

--- src/parse/include/booleguru/parse/aiger.hpp
+++ src/parse/include/booleguru/parse/aiger.hpp
@@ -7,6 +7,19 @@
 
 namespace booleguru::parse {
 class aiger : public base {
+  public:
+  struct gate {
+    bool inverted = false;
+    unsigned l = 0;
+    unsigned r = 0;
+    uint32_t var_id;
+    inline bool is_and() const { return r != 0; }
+    inline bool is_var() const { return r == 0; }
+
+    gate() = default;
+  };
+
+  private:
   unsigned maximum_variable_index_ = 0;
   unsigned number_of_inputs_ = 0;
   unsigned number_of_latches_ = 0;
@@ -18,27 +31,12 @@ class aiger : public base {
     return number_of_inputs_ + number_of_latches_;
   }
 
-  struct and_ {
-    unsigned l = 0;
-    unsigned r = 0;
-  };
-  struct var_ {
-    // At first, this is just 0 or 1 (1 means notted variable). Once symbols are
-    // added, variables are resolved to entries in the variable manager.
-    unsigned var_ = 0;
-    inline unsigned notted() const { return var_ & 0b1; }
-  };
-  struct gate {
-    union {
-      and_ a;
-      var_ v;
-    };
-    inline bool is_and() const { return a.r == 0; }
-    inline bool is_var() const { return a.r != 0; }
-  };
   std::vector<gate> gates_;
 
-  result parse_aag();
+  unsigned left(unsigned var) const;
+  unsigned right(unsigned var) const;
+
+  result parse_aag(std::istringstream &header);
   result parse_aig();
 
   /** Save a symbol in the symbol table, remember the variable it resolves
--- src/parse/src/aiger.cpp
+++ src/parse/src/aiger.cpp
@@ -2,35 +2,61 @@
 #include <sstream>
 #include <stack>
 
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+
 #include <booleguru/expression/op_manager.hpp>
 #include <booleguru/expression/var_manager.hpp>
 #include <booleguru/parse/aiger.hpp>
 #include <booleguru/parse/result.hpp>
 
+template<>
+struct fmt::formatter<booleguru::parse::aiger::gate> {
+  constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
+  template<typename Context>
+  constexpr auto format(booleguru::parse::aiger::gate const& g,
+                        Context& ctx) const {
+    char sign = ' ';
+    if(g.inverted)
+      sign = '-';
+
+    if(g.is_and())
+      return format_to(ctx.out(), "{}(and {} {})", sign, g.l, g.r);
+    else
+      return format_to(ctx.out(), "{}(var)", sign);
+  }
+};
+
+using fmt::println;
+
 namespace booleguru::parse {
+
 result
-aiger::parse_aag() {
+aiger::parse_aag(std::istringstream& header) {
   // The header contains all required meta-information.
-  if((in_ >> maximum_variable_index_).fail())
+  if((header >> maximum_variable_index_).fail())
     return error("could not extract maximum variable index", 2);
-  if((in_ >> number_of_inputs_).fail())
+  if((header >> number_of_inputs_).fail())
     return error("could not extract number of inputs", 2);
-  if((in_ >> number_of_latches_).fail())
+  if((header >> number_of_latches_).fail())
     return error("could not extract number of latches", 2);
-  if((in_ >> number_of_outputs_).fail())
+  if((header >> number_of_outputs_).fail())
     return error("could not extract number of outputs", 2);
-  if((in_ >> number_of_and_gates_).fail())
+  if((header >> number_of_and_gates_).fail())
     return error("could not extract number of and gates", 2);
 
   if(number_of_latches_ > 0) {
     return error("latches not supported", 4);
   }
 
+  gates_.resize(maximum_variable_index_ + 1);
+
   // The format itself is line-based.
   std::string line_buf;
   std::string symbol;
-  while(std::getline(in_, line_buf) && line_buf.length() > 0) {
-    std::istringstream line{ line_buf };
+  while(std::getline(in_, line_buf)) {
+    println("Line: {}", line_buf);
+    std::istringstream line(line_buf);
     // Three possibilities: A single literal, an AND gate (three literals) or a
     // symbol (one symbol / name and one literal).
     int l1 = 0;
@@ -69,17 +95,32 @@ aiger::parse_aag() {
       if((line >> l2).fail()) {
         // We just have a symbol, i.e. an input or output definition (maybe with
         // a not).
-      }
-      if((line >> l3).fail()) {
+        unsigned var = l1 >> 1u;
+        if(var > maximum_variable_index_)
+          return error(
+            fmt::format("variable {} bigger than max variable index {}",
+                        var,
+                        maximum_variable_index_));
+        if(var <= number_of_inputs_) {// Input line
+          gates_[var].inverted = (l1 & 0b1u) == 0b1u;
+        } else if(var <= number_of_inputs_ + number_of_latches_) {// latch lines
+          return error("latches not supported", 4);
+        } else if(var <= number_of_inputs_ + number_of_latches_ +
+                           number_of_outputs_) {// output lines
+          gates_[var].inverted = (l1 & 0b1u) == 0b1u;
+        }
+      } else if((line >> l3).fail()) {
         // We have a latch.
         return error("latches not supported", 4);
       } else {
-        // We have an AND gate.
+        unsigned var = l1 >> 1u;
+        gates_[var].l = l2;
+        gates_[var].r = l3;
       }
     }
   }
 
-  return error("not fully implemented yet");
+  return build();
 }
 result
 aiger::parse_aig() {
@@ -92,11 +133,7 @@ aiger::remember_symbol(unsigned gate, const std::string& symbol) {
   // at the end. This means we have to save up all the symbols we want to swap
   // and finally swap all variables in one go.
   uint32_t var_id = vars_->get_id(expression::variable{ symbol });
-  uint32_t varop_id =
-    ops_->get_id(expression::op{ expression::op_type::Var, var_id, 0 });
-  assert(gates_[gate].is_var());
-  unsigned notted = gates_[gate].v.notted();
-  gates_[gate].v.var_ = (varop_id << 1) | notted;
+  gates_[gate].var_id = var_id;
 }
 
 result
@@ -106,28 +143,85 @@ aiger::build() {
   } else if(number_of_outputs_ == 1) {
     // This is the archetypical formula of one output and many inputs. Build it
     // like a boolean expression by operating on two stacks.
-    unsigned root = outputs_offset();
+    unsigned root = outputs_offset() + 1;
     std::stack<unsigned> s;
-    std::stack<expression::op_ref::ref> o;
     s.emplace(root);
-    return error("not implemented yet");
+    std::cout << "Root: " << root << ", Gate size: " << gates_.size()
+              << std::endl;
+
+    println("Gates: {}", fmt::join(gates_, ", "));
+
+    unsigned prev = 0;
     while(!s.empty()) {
+      unsigned current = s.top();
+      s.pop();
+      println("Current: {}", current);
+      if(!prev || left(prev) == current || right(prev) == current) {
+        if(auto l = left(current))
+          s.emplace(l >> 1u);
+        else if(auto r = right(current))
+          s.emplace(r >> 1u);
+        else {
+          s.pop();
+
+          // visit current
+          std::cout << current << std::endl;
+        }
+      } else if(left(current) == prev) {
+        if(auto r = right(current)) {
+          s.emplace(r >> 1u);
+        } else {
+          s.pop();
+
+          // visit current
+          std::cout << current << std::endl;
+        }
+      } else if(right(current) == prev) {
+        s.pop();
+
+        std::cout << current << std::endl;
+      }
+      prev = current;
     }
+    return error("not implemented yet");
   } else {
     return error("number of outputs > 1 not supported yet");
   }
   return error("not implemented yet");
 }
 
+unsigned
+aiger::left(unsigned var) const {
+  assert(var < gates_.size());
+  if(gates_[var].is_and())
+    return gates_[var].l;
+  else
+    return 0;
+}
+
+unsigned
+aiger::right(unsigned var) const {
+  assert(var < gates_.size());
+  if(gates_[var].is_and())
+    return gates_[var].r;
+  else
+    return 0;
+}
+
 result
 aiger::operator()() {
+  std::string header_line;
+  if(!std::getline(in_, header_line))
+    return error("cannot read first line");
+  std::istringstream header(header_line);
   std::string format;
-  in_ >> format;
-  in_ >> std::skipws;
+  if((header >> format).fail())
+    return error("cannot read format from first line");
+
   if(format == "aig") {
     return parse_aig();
   } else if(format == "aag") {
-    return parse_aag();
+    return parse_aag(header);
   }
   return error("Invalid AIGER format! Needs to be 'aig' or 'aag'");
 }
