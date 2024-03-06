1a90db75d852164de07b033a92a984e51259ff4c
Max Heisinger <maximilian.heisinger@jku.at>
2024-03-06 11:13:43 +0100

    Remove two passes in plaisted_greenbaum

--- src/transform/include/booleguru/transform/polarity_extractor.hpp
+++ src/transform/include/booleguru/transform/polarity_extractor.hpp
@@ -7,15 +7,17 @@ class op_ref;
 namespace booleguru::transform {
 /** Sets user_flag_4 (seen in positive polarity) and user_flag_5
     (seen in negative polarity).
+
+    Also sets the mark bit for all visited nodes.
 */
 class polarity_extractor {
   public:
   polarity_extractor();
   ~polarity_extractor();
 
-  /// Reset user_flag_4 and user_flag_5 to false. This is the
+  /// Reset mark, user_flag_4, and user_flag_5 to false. This is the
   /// invariant that is needed for the result to make sense.
-  void reset_user_4_5(expression::op_ref o);
+  void reset_user_4_5_mark(expression::op_ref o);
 
   expression::op_ref operator()(expression::op_ref o);
 };
