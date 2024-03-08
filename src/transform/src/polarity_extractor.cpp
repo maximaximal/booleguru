0e8f0c9672516a3931801362e0954b0ccb12eed0
Max Heisinger <maximilian.heisinger@jku.at>
2024-03-08 13:25:29 +0100

    Fix polarity extractor to extract correctly for ->,<-,<->,^

--- src/transform/src/polarity_extractor.cpp
+++ src/transform/src/polarity_extractor.cpp
@@ -41,21 +41,27 @@ polarity_extractor::operator()(expression::op_ref o) {
       case None:
         assert(false);
         break;
+      case Equi:
+        [[fallthrough]];
+      case Xor:
+        s.emplace(std::make_pair(op.left(), nots + 1));
+        s.emplace(std::make_pair(op.right(), nots + 1));
+	break;
+      case Impl:
+        s.emplace(std::make_pair(op.left(), nots + 1));
+        s.emplace(std::make_pair(op.right(), nots));
+	break;
+      case Lpmi:
+        s.emplace(std::make_pair(op.left(), nots));
+        s.emplace(std::make_pair(op.right(), nots + 1));
+	break;
       case Exists:
         [[fallthrough]];
       case Forall:
         [[fallthrough]];
-      case Equi:
-        [[fallthrough]];
-      case Impl:
-        [[fallthrough]];
-      case Lpmi:
-        [[fallthrough]];
       case Or:
         [[fallthrough]];
       case And:
-        [[fallthrough]];
-      case Xor:
         s.emplace(std::make_pair(op.left(), nots));
         s.emplace(std::make_pair(op.right(), nots));
         break;
